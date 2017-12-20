9bc375c66bc8b90dd38bfa1f3df8affd9153b92c
littlepai <txgyswh@163.com>
2017-12-20 18:47:13 +0800

    模块化模型

--- code/pai/deeplearn_train.py
+++ code/pai/deeplearn_train.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import math
 import tensorflow as tf
 from tensorflow.python.training import moving_averages
 import cx_Oracle as cx
@@ -73,126 +74,168 @@ x_columns = ['DAYS_DISTANCE',
  'WEEKDAY7']
 target='LABEL'
 
+tf.app.flags.DEFINE_string('mode', 'train', "模式，可选训练train，其他")
+tf.app.flags.DEFINE_float('beta1', 0.9, "AdamOptimizer 参数")
+tf.app.flags.DEFINE_float('beta2', 0.999, "AdamOptimizer 参数")
+tf.app.flags.DEFINE_float('learning_rate', 1e-5, "学习率")
+tf.app.flags.DEFINE_integer('batch_size', 128, "最小批大小")
 
-mode='train'
-_extra_train_ops=[]
+FLAGS=tf.app.flags.FLAGS
 
-def _batch_norm(name, x):
-        """批标准化."""
-        with tf.variable_scope(name):
-            params_shape = [x.get_shape()[-1]] #获取tensor的最后一个维度，后面的均值，方差都是这个维度
-            
-            # 标准化数据为均值为0方差为1之后，还有一个x=x*gamma+beta的调整
-            # 这个会随着训练不断调整
-            beta = tf.get_variable(
-                'beta', params_shape, tf.float32,
-                initializer=tf.constant_initializer(0.0, tf.float32))
-            gamma = tf.get_variable(
-                'gamma', params_shape, tf.float32,
-                initializer=tf.constant_initializer(1.0, tf.float32))
-            
-            # 训练的时候不断调整平滑均值，平滑方差
-            # 预测的时候，回复权重使用的是训练过程中调整出来的平滑方差均值去做标准化
-            if mode == 'train':
-                mean, variance = tf.nn.moments(x, [0], name='moments') #获取批均值和方差，size[最后一个维度]
-                
-                # moving_mean, moving_variance 这两个name一定要让训练和预测的时候都相等，不然就没法恢复训练好的值了
-                moving_mean = tf.get_variable(
-                    'moving_mean', params_shape, tf.float32,
-                    initializer=tf.constant_initializer(0.0, tf.float32),
-                    trainable=False)
-                moving_variance = tf.get_variable(
-                    'moving_variance', params_shape, tf.float32,
-                    initializer=tf.constant_initializer(1.0, tf.float32),
-                    trainable=False)
+class O2Omodel():
+	def __init__(self):
+		self.inputs = tf.placeholder(tf.float32, [None, 67])
+		self.labels = tf.placeholder(tf.float32, [None, 2])
+		self.mode=FLAGS.mode
+		self._extra_train_ops=[]
+	
+	def build_graph(self):
+		self._build_model()
+		self._build_train_op()
+		
+	
+	def _build_model(self):
+		w={
+		'w1':tf.get_variable('w1', shape=(67, 64), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer()),
+		'w2':tf.get_variable('w2', shape=(64, 128), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer()),
+		'w3':tf.get_variable('w3', shape=(128, 32), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer()),
+		'w4':tf.get_variable('w4', shape=(32, 2), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+		}
 
-                _extra_train_ops.append(moving_averages.assign_moving_average(
-                    moving_mean, mean, 0.9))
-                _extra_train_ops.append(moving_averages.assign_moving_average(
-                    moving_variance, variance, 0.9))
-            else:
-                # mean的name一定要跟train的时候的一样 moving_mean, 这样在restore的时候才可以加载训练的时候的值
-                mean = tf.get_variable(
-                    'moving_mean', params_shape, tf.float32,
-                    initializer=tf.constant_initializer(0.0, tf.float32),
-                    trainable=False)
-                # variance的name一定要跟train的时候的一样 moving_variance
-                variance = tf.get_variable(
-                    'moving_variance', params_shape, tf.float32,
-                    initializer=tf.constant_initializer(1.0, tf.float32),
-                    trainable=False)
-                
-                # 可视化
-                # tf.summary.histogram(mean.op.name, mean)
-                # tf.summary.histogram(variance.op.name, variance)
-            
-            # 计算，标准化，最后一个值为误差，一般设置很小即可
-            x_bn = tf.nn.batch_normalization(x, mean, variance, beta, gamma, 0.001)
-            x_bn.set_shape(x.get_shape())
+		b={
+		'b1':tf.get_variable('b1', shape=(64), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer()),
+		'b2':tf.get_variable('b2', shape=(128), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer()),
+		'b3':tf.get_variable('b3', shape=(32), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer()),
+		'b4':tf.get_variable('b4', shape=(2), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
+		}
 
-            return x_bn
+		x=tf.matmul(self.inputs, w['w1'])+b['b1']
+		x=self._leaky_relu(x, 0.01)
+		x=tf.matmul(x, w['w2'])+b['b2']
+		x=self._leaky_relu(x, 0.01)
+		x=tf.matmul(x, w['w3'])+b['b3']
+		x=self._leaky_relu(x, 0.01)
+		self.x=tf.matmul(x, w['w4'])+b['b4']
+		
+		
+		
+	def _build_train_op(self):
+		self.output = tf.nn.softmax(self.x)
+		self.pred=tf.argmax(self.output, axis=1)
+		self.loss=tf.nn.softmax_cross_entropy_with_logits(logits=self.x, labels=self.labels) #这里的logits参数要接收“未经过激活函数的输出”
+		self.cost=tf.reduce_mean(self.loss)
+		
+		self.global_step = tf.Variable(0, trainable=False)
+		self.optimizer = tf.train.AdamOptimizer(learning_rate=FLAGS.learning_rate,beta1=FLAGS.learning_rate,beta2=FLAGS.learning_rate).minimize(self.loss,global_step=self.global_step)
+		self.train_ops = [self.global_step, self.cost, self.optimizer] + self._extra_train_ops
+		self.train_op = tf.group(*self.train_ops)
+	
+	def _batch_norm(self, name, x):
+		"""批标准化."""
+		with tf.variable_scope(name):
+			params_shape = [x.get_shape()[-1]] #获取tensor的最后一个维度，后面的均值，方差都是这个维度
+			
+			# 标准化数据为均值为0方差为1之后，还有一个x=x*gamma+beta的调整
+			# 这个会随着训练不断调整
+			beta = tf.get_variable(
+				'beta', params_shape, tf.float32,
+				initializer=tf.constant_initializer(0.0, tf.float32))
+			gamma = tf.get_variable(
+				'gamma', params_shape, tf.float32,
+				initializer=tf.constant_initializer(1.0, tf.float32))
+			
+			# 训练的时候不断调整平滑均值，平滑方差
+			# 预测的时候，回复权重使用的是训练过程中调整出来的平滑方差均值去做标准化
+			if self.mode == 'train':
+				mean, variance = tf.nn.moments(x, [0], name='moments') #获取批均值和方差，size[最后一个维度]
+				
+				# moving_mean, moving_variance 这两个name一定要让训练和预测的时候都相等，不然就没法恢复训练好的值了
+				moving_mean = tf.get_variable(
+					'moving_mean', params_shape, tf.float32,
+					initializer=tf.constant_initializer(0.0, tf.float32),
+					trainable=False)
+				moving_variance = tf.get_variable(
+					'moving_variance', params_shape, tf.float32,
+					initializer=tf.constant_initializer(1.0, tf.float32),
+					trainable=False)
+				self._extra_train_ops.append(moving_averages.assign_moving_average(
+					moving_mean, mean, 0.9))
+				self._extra_train_ops.append(moving_averages.assign_moving_average(
+					moving_variance, variance, 0.9))
+			else:
+				# mean的name一定要跟train的时候的一样 moving_mean, 这样在restore的时候才可以加载训练的时候的值
+				mean = tf.get_variable(
+					'moving_mean', params_shape, tf.float32,
+					initializer=tf.constant_initializer(0.0, tf.float32),
+					trainable=False)
+				# variance的name一定要跟train的时候的一样 moving_variance
+				variance = tf.get_variable(
+					'moving_variance', params_shape, tf.float32,
+					initializer=tf.constant_initializer(1.0, tf.float32),
+					trainable=False)
+				
+				# 可视化
+				# tf.summary.histogram(mean.op.name, mean)
+				# tf.summary.histogram(variance.op.name, variance)
+			
+			# 计算，标准化，最后一个值为误差，一般设置很小即可
+			x_bn = tf.nn.batch_normalization(x, mean, variance, beta, gamma, 0.001)
+			x_bn.set_shape(x.get_shape())
+			return x_bn
 
-def _max_pool(x, ksize=2, strides=1):
-    return tf.nn.max_pool(x,ksize=[1, ksize, ksize, 1],strides=[1, strides, strides, 1],padding='SAME',name='max_pool')
+	def _max_pool(self, x, ksize=2, strides=1):
+		return tf.nn.max_pool(x,ksize=[1, ksize, ksize, 1],strides=[1, strides, strides, 1],padding='SAME',name='max_pool')
 
-def _leaky_relu(x, leakiness=0.0):
-    return tf.where(tf.less(x, 0.0), leakiness * x, x, name='leaky_relu')
+	def _leaky_relu(self, x, leakiness=0.0):
+		return tf.where(tf.less(x, 0.0), leakiness * x, x, name='leaky_relu')
 
+model=O2Omodel()
+model.build_graph()
 
+class DataIterator():
+	def __init__(self, sql_code='select * from wepon_d1'):
+		conn=cx.connect('coupon/coupon@pai_db')
+		df=pd.read_sql(sql_code, conn)
+		X1=df[x_columns]
+		y1=df[target]
 
-inputs = tf.placeholder(tf.float32, [None, 67])
-y = tf.placeholder(tf.float32, [None, 2])
+		X1=X1.applymap(lambda x: 0 if x is None else x)
+		X1=X1.applymap(lambda x: 0 if np.isnan(x) else x)
+		X1=X1.applymap(lambda x: x*1.0)
 
+		self.datas=np.asanyarray(X1, np.float32)
+		self.labels=np.zeros([len(y1), 2])
+		self.labels[:,0]=np.where(y1==0,1,0)
+		self.labels[:,1]=np.where(y1==1,1,0)
+	
+	@property
+	def size(self):
+		return len(self.labels)
 
-w={
-'w1':tf.get_variable('w1', shape=(67, 64), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer()),
-'w2':tf.get_variable('w2', shape=(64, 128), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer()),
-'w3':tf.get_variable('w3', shape=(128, 32), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer()),
-'w4':tf.get_variable('w4', shape=(32, 2), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
-}
+	
+	def get_data_by_indexs(self, indexs):
+		return self.datas[indexs], self.labels[indexs]
 
-b={
-'b1':tf.get_variable('b1', shape=(64), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer()),
-'b2':tf.get_variable('b2', shape=(128), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer()),
-'b3':tf.get_variable('b3', shape=(32), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer()),
-'b4':tf.get_variable('b4', shape=(2), dtype=tf.float32, initializer=tf.contrib.layers.xavier_initializer())
-}
+	def gen_shuffle_batch(self, batch_size):
+		while(True):
+			self.shuffle_idx=np.random.permutation(self.size)
+			self.total_epoch=math.ceil(self.size/batch_size)
+			for i in range(self.total_epoch):
+				yield self.get_data_by_indexs(self.shuffle_idx[i*batch_size: (i+1)*batch_size if (i+1)*batch_size < self.size else self.size])
 
-x=tf.matmul(inputs, w['w1'])+b['b1']
-x=_leaky_relu(x, 0.01)
-x=tf.matmul(x, w['w2'])+b['b2']
-x=_leaky_relu(x, 0.01)
-x=tf.matmul(x, w['w3'])+b['b3']
-x=_leaky_relu(x, 0.01)
-x=tf.matmul(x, w['w4'])+b['b4']
-output = tf.nn.softmax(x)
-pred=tf.argmax(output, axis=1)
-loss=tf.nn.softmax_cross_entropy_with_logits(logits=x, labels=y)
-cost=tf.reduce_mean(loss)
+def main(_):
+	sess=tf.Session()
+	init=tf.global_variables_initializer()
+	sess.run(init)
+	data=DataIterator()
+	gen=data.gen_shuffle_batch(FLAGS.batch_size)
+	for i in range(10000):
+		
+		inputs, labels=next(gen)
+		print(sess.run(model.train_ops, feed_dict={model.inputs:inputs, model.labels:labels}))
 
-conn=cx.connect('coupon/coupon@pai_db')
-sql_code1='select * from wepon_d1'
-df1=pd.read_sql(sql_code1, conn)
-X1=df1[x_columns]
-y1=df1[target]
+if __name__ == "__main__":
+	tf.app.run()
 
-X1=X1.applymap(lambda x: 0 if x is None else x)
-X1=X1.applymap(lambda x: 0 if np.isnan(x) else x)
-
-X1=X1.applymap(lambda x: x*1.0)
-X=np.asanyarray(X1, np.float32)
-label=np.zeros([len(y1), 2])
-label[:,0]=np.where(y1==0,1,0)
-label[:,1]=np.where(y1==1,1,0)
-
-
-global_step = tf.Variable(0, trainable=False)
-optimizer = tf.train.AdamOptimizer(learning_rate=1e-5,beta1=0.9,beta2=0.999).minimize(loss,global_step=global_step)
-train_ops = [global_step, cost, optimizer] + _extra_train_ops
-train_op = tf.group(*train_ops)
-
-sess=tf.Session()
-init=tf.global_variables_initializer()
-sess.run(init)
-#sess.run(train_op, feed_dict={inputs:X, y:label})
-#sess.run(loss, feed_dict={inputs:X, y:label})
+#sess.run(model.train_op, feed_dict={model.inputs:X, model.labels:label})
+#sess.run(model.loss, feed_dict={model.inputs:X, model.labels:label})
